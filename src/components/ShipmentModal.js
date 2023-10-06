d9be467441a07a151b894c1e4f87984cf6d3c667
Kadi <markvoru@gmail.com>
2023-10-06 15:53:18 +0300

    modal css update

--- src/components/ShipmentModal.js
+++ src/components/ShipmentModal.js
@@ -37,12 +37,13 @@ export default function EditShipmentData(props) {
       </Button>
       <Modal
         show={show}
-        onHide={handleClose}>
+        onHide={handleClose}
+        size="xl">
         <Modal.Header closeButton></Modal.Header>
         <Modal.Body>
           <form
             id="modal"
-            className="w-full max-w-lg">
+            className="w-full max-w-xxl">
             <div className="flex flex-wrap -mx-3 mb-6">
               <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                 <label
